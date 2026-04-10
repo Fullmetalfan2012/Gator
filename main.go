78b27e3a9b82b3bc628634f60c91e2d6eb7d2e26
Fullmetalfan2012 <fullmetalfan2012@gmail.com>
2026-04-11 00:42:03 +0200

    Changed a lot

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
+
 	"github.com/Fullmetalfan2012/Gator/internal/config"
+	"github.com/Fullmetalfan2012/Gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,6 +19,16 @@ func main() {
 	s.cfg = &cfg
 	cmds := commands{cmds: make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+
+	//DB Handling
+	db, err := sql.Open("postgres", s.cfg.DbUrl)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+	s.db = database.New(db)
+
 	input := os.Args
 	if len(input) < 2 {
 		log.Fatalf("Too few arguments")
--- repl.go
+++ repl.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"time"
+
 	"github.com/Fullmetalfan2012/Gator/internal/config"
+	"github.com/Fullmetalfan2012/Gator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,11 +25,41 @@ func handlerLogin(s *state, cmd command) error {
 	if cmd.args == nil || len(cmd.args) != 1 {
 		return fmt.Errorf("Error: Login expects one argument: username")
 	}
-	err := s.cfg.SetUser(cmd.args[0]) 
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		fmt.Println("Error: user does not exist")
+		os.Exit(1)
+	}
+	err = s.cfg.SetUser(cmd.args[0])
 	if err != nil {
 		return fmt.Errorf("Error setting user in login handler: %w", err)
 	}
 	fmt.Println("User has been set.")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if cmd.args == nil || len(cmd.args) != 1 {
+		return fmt.Errorf("Error: Register expects one argument: name")
+	}
+	name := cmd.args[0]
+	now := time.Now()
+	usr, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	})
+	if err != nil {
+		fmt.Printf("Error: user with name '%s' already exists\n", name)
+		os.Exit(1)
+	}
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("Error setting current user: %w", err)
+	}
+	fmt.Printf("User created successfully!\n")
+	fmt.Printf("User data: %+v\n", usr)
 	return nil
 }
 
@@ -40,6 +77,16 @@ func (c *commands) run(s *state, cmd command) error {
 		return fmt.Errorf("Error running command %v: %w", cmd.name, err)
 	}
 	return nil	
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error resetting database: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Database reset successfully.")
+	return nil
 }
 
 func (c *commands) register(name string, f func(*state, command) error) {
